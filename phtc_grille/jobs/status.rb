e15885f0ff720f7cd19e64042498cc0412dd59ea
jonpetersen <jonepetersen@gmail.com>
2018-02-19 11:18:59 +0000

    added forever.json for crontab reboot

--- phtc_grille/jobs/status.rb
+++ phtc_grille/jobs/status.rb
@@ -1,11 +1,18 @@
 require 'uri'
 
-SCHEDULER.every '1s' do
+SCHEDULER.every '3s' do
 
   uri = URI('http://127.0.0.1:9292/status')
   current_status = JSON.parse(Net::HTTP.get(uri))["status"]
-  uri = URI('http://127.0.0.1:9292/appstatus')
-  current_appstatus = JSON.parse(Net::HTTP.get(uri))["appstatus"]
+  
+  appstatusport_is_open = Socket.tcp("127.0.0.1", 3000, connect_timeout: 5) { true } rescue false
+  if appstatusport_is_open
+    uri = URI('http://127.0.0.1:3000/appstatus') #nodejs URI
+    current_appstatus = Net::HTTP.get(uri)
+  else
+    current_appstatus = "unkown"
+  end
+  
   send_event('appstatus', { appstatus: current_appstatus})
   send_event('hotstatus', { status: current_status})
   #send_event('hotstatus_snapshot', { message: "Some message", status:"ok" })
